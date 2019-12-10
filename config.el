9133dcdfadb49ff5950f7fd67a240a1b56e5f07a
dp12 <deep.paren.12@gmail.com>
2019-12-10 18:07:50 -0500

    Added preliminary packages and keybindings

--- config.el
+++ config.el
@@ -1,3 +1,50 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
+(setq doom-font (font-spec :family "Fixedsys Excelsior 301" :size 20))
+
 ;; Place your private configuration here
+(map! :leader
+      :desc "avy-kill-whole-line" "ak" #'avy-kill-whole-line
+      :desc "avy-kill-region" "aK" #'avy-kill-region
+      :desc "avy-copy-line" "ay" #'avy-copy-line
+      :desc "align-regexp" "ax" #'align-regexp
+      :desc "bookmark jump" "bj" #'counsel-bookmark
+      :desc "list bookmarks" "bl" #'list-bookmarks
+      :desc "swap windows" "bx" #'ace-swap-window
+      :desc "copy and comment" "ck" #'evilnc-comment-and-kill-ring-save
+      :desc "dired-omit-mode" "d." #'dired-omit-mode
+      :desc "dumb jump other" "dG" #'dumb-jump-go-other-window
+      :desc "dumb jump" "dg" #'dumb-jump-go
+      :desc "dired-jump" "dj" #'dired-jump
+      :desc "kill dired" "dk" #'kill-dired-buffers
+      :desc "delete-matching-lines" "dm" #'delete-matching-lines
+      :desc "dumb jump quick look" "dl" #'dumb-jump-quick-look
+      :desc "dired jump other" "do" #'dired-jump-other-window
+      :desc "ediff-buffers" "ed" #'ediff-buffers
+      :desc "evil-show-marks" "em" #'evil-show-marks
+      :desc "evil-show-registers" "er" #'evil-show-registers
+      :desc "counsel-file-jump" "fJ" #'counsel-file-jump
+      :desc "projectile-ripgrep-filename" "fN" #'projectile-ripgrep-filename
+      :desc "frog-jump-buffer" "fb" #'frog-jump-buffer
+      :desc "copy-file-basename-to-clipboard" "fn" #'copy-file-basename-to-clipboard
+      :desc "fzf-git-files" "fz" #'fzf-git-files
+      :desc "gdb-set-fast-breakpoint" "gd" #'gdb-set-fast-breakpoint
+      :desc "counsel-ffap-git" "gff" #'counsel-ffap-git
+      :desc "ggtags-find-reference" "gr" #'ggtags-find-reference
+      :desc "highlight-symbol-at-point" "h." #'highlight-symbol-at-point
+      :desc "unhighlight-regexp" "hr" #'unhighlight-regexp
+      :desc "hexl-mode" "hx" #'hexl-mode
+      :desc "counsel-recentf" "if" #'counsel-recentf
+      :desc "mu4e update mail cnt" "ma" #'mu4e-alert-update-mail-count-interactive
+      :desc "evil-mc-mode" "mc" #'evil-mc-mode
+      :desc "magit-find-file" "mf" #'magit-find-file
+      :desc "mu4e-jump-to-mail" "mj" #'mu4e-jump-to-mail
+      :desc "magit-file-popup" "mp" #'magit-file-popup
+      :desc "mu4e-update-mail" "mu" #'mu4e-update-mail-and-index-wrapper
+      :desc "toggle-org-checkbox" "oc" #'toggle-org-checkbox
+      :desc "counsel-org-goto" "og" #'counsel-org-goto
+      :desc "projectile-ripgrep-custom" "rg" #'projectile-ripgrep-custom
+      :desc "parrot-start-animation" "rr" #'parrot-start-animation
+      :desc "counsel-yank-pop" "ry" #'counsel-yank-pop
+      :desc "sort-lines" "sl" #'sort-lines
+      )
--- packages.el
+++ packages.el
@@ -5,3 +5,22 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
+
+(package! projectile-ripgrep)
+(package! deadgrep)
+(package! fzf)
+(package! frog-jump-buffer)
+(package! company-try-hard)
+(package! shell-pop)
+(package! quickrun)
+
+(package! elsa)
+(package! flycheck-elsa)
+(package! flycheck-package)
+(package! package-lint)
+(package! ox-hugo)
+(package! dired-git-info)
+(package! source-peek :recipe (:host github :repo "iqbalansari/emacs-source-peek"))
+
+(package! parrot)
+(package! double-saber)
