76ec16b1b3b667a10f0187101ff9cc5d0b5288cd
dp12 <deep.paren.12@gmail.com>
2019-12-11 16:34:29 -0500

    Turn off truncate lines

--- config.el
+++ config.el
@@ -9,6 +9,7 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+(toggle-truncate-lines -1) ;; wrap long lines
 
 ;;; Leader keybindings
 (map! :leader
