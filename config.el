5f7911eaf59c346b02adbd0397aa7b76f6b15abb
dp12 <deep.paren.12@gmail.com>
2019-12-12 15:56:16 -0500

    Add spacemacs-like winum keybindings

--- config.el
+++ config.el
@@ -77,6 +77,16 @@
       :desc "evil-iedit" "se" #'evil-iedit-state/iedit-mode
       :desc "truncate-lines" "tl" #'toggle-truncate-lines
       :desc "toggle-line-numbers" "tn" #'doom/toggle-line-numbers
+      "0" #'winum-select-window-0
+      "1" #'winum-select-window-1
+      "2" #'winum-select-window-2
+      "3" #'winum-select-window-3
+      "4" #'winum-select-window-4
+      "5" #'winum-select-window-5
+      "6" #'winum-select-window-6
+      "7" #'winum-select-window-7
+      "8" #'winum-select-window-8
+      "9" #'winum-select-window-9
       )
 
 ;;; Non-leader keybindings
