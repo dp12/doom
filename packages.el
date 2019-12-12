fd464bb5791fdb9a51f147cb2b20fa05966a1c26
dp12 <deep.paren.12@gmail.com>
2019-12-12 10:46:12 -0500

    Reorganize packages

--- packages.el
+++ packages.el
@@ -6,29 +6,36 @@
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
+;;; Extensions for built-in tools
+(package! dired-git-info)
+(package! dired-narrow)
+(package! frog-jump-buffer)
+(package! key-chord)
+(package! ox-hugo)
+(package! shell-pop)
+
+;;; Search
+(package! deadgrep)
+(package! double-saber)
+(package! fzf)
 (package! ggtags)
 (package! projectile-ripgrep)
-(package! deadgrep)
-(package! fzf)
-(package! frog-jump-buffer)
-(package! company-try-hard)
-(package! shell-pop)
-(package! quickrun)
 
-(package! symbol-overlay)
-(package! evil-iedit-state)
-(package! imenu-list)
+;;; Emacs lisp development
 (package! elsa)
 (package! flycheck-elsa)
 (package! flycheck-package)
 (package! package-lint)
-(package! ox-hugo)
-(package! dired-git-info)
-(package! source-peek :recipe (:host github :repo "iqbalansari/emacs-source-peek"))
 
+;;; Programming tools
+(package! company-try-hard)
 (package! corral)
-(package! key-chord)
+(package! evil-iedit-state)
+(package! imenu-list)
+(package! quickrun)
+(package! source-peek :recipe (:host github :repo "iqbalansari/emacs-source-peek"))
+(package! symbol-overlay)
+
+;;; Appearance
 (package! nyan-mode)
 (package! parrot)
-(package! double-saber)
-
